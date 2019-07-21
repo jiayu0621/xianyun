90139cb534bed02d96b06499789dd2da6d4d18cb
zjs <1262474673@qq.com>
2019-07-22 02:02:30 +0800

    地图模块布局完成

---
+++ components/hotel/hotelMap.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hotel_map">
+      
+    <el-row class="hotel_map_content">
+      <el-col :span="15" id="container" ></el-col>
+      <el-col :span="9" class="container_item"></el-col>
+    </el-row>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data:{
+      type:Object,
+      default:{},
+    }
+  },
+  mounted() {
+    
+    //   页面加载之后执行
+    window.onLoad = function() {
+      var map = new AMap.Map("container", {
+        zoom: 19, //级别
+        center: [data.location.longitude, data.location.latitude], //中心点坐标
+        viewMode: "3D" //使用3D视图
+      });
+
+      var marker = new AMap.Marker({
+    position: new AMap.LngLat(data.location.longitude,data.location.latitude),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+    title: '北京'
+});
+map.add(marker);
+    }
+    var key = "7325822086d3558de6783270b2f52caa";
+    // 为什么不能加双引号
+    var url = `https://webapi.amap.com/maps?v=1.4.15&key=${ key}&callback=onLoad`;
+    var jsapi = document.createElement("script");
+    jsapi.charset = "utf-8";
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.hotel_map{
+  margin: 30px 0;
+#container {
+  height: 400px;
+}
+}
+</style>
--- pages/hotel/hotelDetail.vue
+++ pages/hotel/hotelDetail.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="hotel_detail">
     <div class="header_link">
-      <nuxt-link to>酒店</nuxt-link>&nbsp;&nbsp;>
-      <nuxt-link to>南京酒店</nuxt-link>&nbsp;&nbsp;>
-      <span class="default_color">好来阁商务宾馆</span>
+      <nuxt-link to='/hotel'>酒店</nuxt-link>&nbsp;&nbsp;>
+      <nuxt-link to='/hotel'>南京酒店</nuxt-link>&nbsp;&nbsp;>
+      <span class="default_color">{{hotelDetail.name}}</span>
     </div>
     <div class="hotel_name">
       <h2 class="title_name">
-        好来阁商务宾馆
+        {{hotelDetail.name}}
         <span class="title_grade">
           <i class="iconfont iconhuangguan"></i>
           <i class="iconfont iconhuangguan"></i>
@@ -16,23 +16,67 @@
           <i class="iconfont iconhuangguan"></i>
         </span>
       </h2>
-      <span class="default_color hotel_EnglishName">hao lai ge shang wu hotel</span>
+      <span class="default_color hotel_EnglishName">{{hotelDetail.alias}}</span>
       <p class="default_color hotel_address">
         <i class="iconfont iconlocation"></i>
-        高淳县淳溪镇镇兴路118号(高淳县委党校对面)
+        {{hotelDetail.address}}
       </p>
     </div>
     <div class="hotel_img">
-        <el-row type='flex' class="hotel_img_row">
-            <el-col :span='14' class="bgImg">
-                <img src="" alt="">
+      <el-row type="flex" justify="space-between" class="hotel_img_row">
+
+        <el-col :span="14" class="bgImg">
+          <!-- <img :src='hotelDetail.pics[0].url' alt =''/> -->
+        </el-col>
+        <el-col :span="10" class="smImg">
+          <el-row
+          
+            class="smImg_row"
+            v-for="(item,index) in hotelDetail.pics"
+            :key="index"
+            >
+            <el-col :span="12" class="smImg_col">
+              <!-- <img :src="$axios.defaults.baseURL+item.url" alt=""> -->
+              <!-- <img :src="`${$axios.defaults.baseURL+item.url}`" alt=""> -->
             </el-col>
-            <el-col :span='10' class="smImg">
-                <el-row type='flex' class="smImg_row">
-                    <el-col :span='12' class="smImg_col"></el-col>
-                    <el-col :span='12' class="smImg_col"></el-col>
-                </el-row>
-                <img src="" alt="">
+          </el-row>
+         
+        </el-col>
+      </el-row>
+    </div>
+    <div class="hotel_item default_color">
+      <el-table style="width: 100%"  :data="hotelDetail.products">
+        <el-table-column prop="name" label="价格来源" ></el-table-column>
+        <el-table-column prop="bestType" label="低价房型" ></el-table-column>
+        <el-table-column prop="price" label="最低价格/每晚" width="180"></el-table-column>
+      </el-table>
+    </div>
+    <!-- 地图组件 -->
+    <HotelMap :data='hotelDetail'/>
+
+    <div class="hotel_info">
+        <el-row>
+            <el-col :span='4'>基本信息</el-col>
+            <el-col :span='20'>
+                <span>入住时间</span>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span='4'>主要设施</el-col>
+            <el-col :span='20'>
+                <span>外币兑换服务</span>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span='4'>停车服务</el-col>
+            <el-col :span='20'>
+                <span>可以停车</span>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span='4'>品牌信息</el-col>
+            <el-col :span='20'>
+                <span>有品牌 </span>
             </el-col>
         </el-row>
     </div>
@@ -40,7 +84,44 @@
 </template>
 
 <script>
-export default {};
+import HotelMap from '@/components/hotel/hotelMap'
+export default {
+  components: {
+    HotelMap,
+  },
+    data () {
+        return {
+            // hotelInfo:[
+            //     { form:'携程', type:'高级大床房', price:'230', }
+            // ],
+            hotelDetail:{
+              location:{}
+            }
+        }
+    },
+    mounted () {
+        this.$axios({
+            url:'/hotels',
+            method:'get',
+            params:{
+                id:1
+            }
+        }).then(res=>{
+            // console.log(res);
+            const {data} = res.data
+            this.hotelDetail=data[0]
+            // console.log(this.hotelDetail);
+        })
+          this.$axios({
+            url:'/hotels/options',
+            method:'get',
+          
+        }).then(res=>{
+            console.log(res);
+        })
+
+    }
+};
 </script>
 
 <style lang='less' scoped>
@@ -62,25 +143,52 @@ export default {};
       padding-left: 8px;
     }
   }
-  .hotel_img{
-      
-      .hotel_img_row{
-          width: 1000px;
-          .bgImg{
-              width: 640px;
-              height: 360px;
-              background-color: lightcoral;
-              margin-right: 20px;
+  .hotel_img {
+    .hotel_img_row {
+      margin: 30px 0;
+      .bgImg {
+        width: 640px;
+        height: 360px;
+        margin-right: 20px;
+        img{
+        // width: 640px;
+        // height: 360px;
+        width: 100%;
+        }
+      }
+      .smImg {
+        //    height: 360px;
+        .smImg_row {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          > div {
+            background-color: aqua;
+            width: 180px;
+            height: 113px;
+            margin-bottom: 10px;
+            // width: 50%;
+            //   box-sizing: border-box;
+            // &:nth-child(odd) {
+              // margin-right: 15px;
+            // }
+           /deep/ img{
+              width: 50%;
+            }
           }
-          .smImg{
-              .smImg_row{
-                  >div{
-                      background-color: aqua;
-                      width: 120px;
-                      height: 120px;
-                      margin: 2px;
-                  }
-              }
+        }
+      }
+    }
+  }
+  .hotel_info{
+      >div{
+          padding: 15px 0;
+          border-bottom: 1px solid #eeeeee;
+          font-size: 14px;
+      }
+      el-row{
+          span{
+              padding: 0 20px;
           }
       }
   }
